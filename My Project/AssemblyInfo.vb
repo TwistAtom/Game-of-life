15676525fa3109c68647a4f37c0bce667b00ee35
Thibault <38385977+TwistAtom@users.noreply.github.com>
2019-06-30 21:36:59 +0200

    Add files via upload

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Les informations générales relatives à un assembly dépendent de
+' l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+' associées à un assembly.
+
+' Vérifiez les valeurs des attributs de l'assembly
+
+<Assembly: AssemblyTitle("Jeu De La Vie")>
+<Assembly: AssemblyDescription("Le jeu de la vie est un automate cellulaire. Le jeu de la vie est un jeu au sens mathématique plutôt que ludique.")>
+<Assembly: AssemblyCompany("Thibault A")>
+<Assembly: AssemblyProduct("Jeu De La Vie")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+<Assembly: Guid("e42f2725-5e73-48c0-9e44-e3e480c916b9")>
+
+' Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+'
+'      Version principale
+'      Version secondaire
+'      Numéro de build
+'      Révision
+'
+' Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+' en utilisant '*', comme indiqué ci-dessous :
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
